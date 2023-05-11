68b22ac4b9518869797c0eaed119acba5bfbc3a7
slouch <32179423+slouchd@users.noreply.github.com>
2023-05-11 09:35:43 +0100

    Consolidation rewrite, testing and new approach

--- celebrate_contributors.py
+++ celebrate_contributors.py
@@ -4,10 +4,20 @@
 import sys
 import json
 import html
+import requests
+import matplotlib.pyplot as plt
 from tabulate import tabulate
 
+# GitHub authentication token and repo
+repo = sys.argv[1]
+token = sys.argv[2]
+
+# Get contributors
+headers = {"Authorization": f"Bearer {token}"}
+res = requests.get(f"https://api.github.com/repos/{repo}/contributors", headers=headers)
+
 # Read contributors from input
-contributors = json.loads(sys.argv[1])
+contributors = res.json()
 
 # Data required for table
 contributor_profile_usernames = [contributor['login'] for contributor in contributors]
@@ -31,3 +41,23 @@ with open('CONTRIBUTORS.md', 'w') as f:
     f.write('## Contributors ✨\n')
     f.write('With thanks to all our contributors!\n\n')
     f.write(html.unescape(contributors_table))
+
+# Generate the graph
+# Extract contributor logins and contributions
+contributor_logins = [contributor['login'] for contributor in contributors]
+contributor_contributions = [contributor['contributions'] for contributor in contributors]
+
+# Generate the graph
+plt.bar(contributor_logins, contributor_contributions)
+plt.xlabel('Contributor')
+plt.ylabel('Contributions')
+plt.title('Contributions by Contributor')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+# Save the graph
+plt.savefig('contributions_graph.png')
+
+# Append graph to markdown file
+with open('CONTRIBUTORS.md', 'a') as f:
+    f.write('\n\n<img src="./contributions_graph.png">')
